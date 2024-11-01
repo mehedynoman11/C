63225cf899d1d8bdb45055de30c1c6b3f0c9efa6
mehedynoman11a <mehedynoman11@gmail.com>
2024-11-01 18:42:55 +0600

    reverse array new technic

---
+++ reversArray.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+
+    // Write your code here.
+    int N;
+    scanf("%d", &N);
+    int arr[N];
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int i=0, j=N-1;
+    while (i < j)
+    {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+        i++;
+        j--;
+    }
+    for (int i = 0; i < N; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    
+    
+    
+
+    return 0;
+}
